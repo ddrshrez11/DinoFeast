b26aac67ae040b3f9795733fcfb809d49bb6ac98
Yunip Shrestha <yunipshrestha@gmail.com>
2019-06-10 01:25:57 +0545

    Character Scaling Increase Completed

---
+++ Assets/Scripts/PickupMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupMovement : MonoBehaviour
+{
+    public int size;
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public int size;
+    public int count;
+    public Text DieText;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        size = 1;
+        count = 0;
+        DieText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            if(other.GetComponent<PickupMovement>().size < size)
+            {
+                 other.gameObject.SetActive(false);
+                 count++;
+                 if (count ==2)
+                {
+                    size++;
+                    transform.localScale = new Vector3(1 * size, 1 * size, 1 * size);
+                    count = 0;
+                }
+            }
+            else
+            {
+                gameObject.SetActive(false);
+                DieText.text = "You Are Dead!";
+            }
+            
+        }
+    }
+}
