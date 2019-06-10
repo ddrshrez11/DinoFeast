e2963d2c3f53514d0cef3be12596a45b33851b98
Yunip Shrestha <yunipshrestha@gmail.com>
2019-06-11 00:05:41 +0545

    Random Movement Type-1

---
+++ Assets/MoveRandomly.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveRandomly : MonoBehaviour
+{
+    public float timer;
+    public NavMeshAgent nav;
+    bool inCoRoutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    Vector3 GetNewRandomPosition ()
+    {
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+
+    IEnumerator DoSomething()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timer);
+        GetNewPath();
+        inCoRoutine = false;
+    }
+
+    void GetNewPath()
+    {
+        nav.SetDestination(GetNewRandomPosition());
+    }
+
+    void Update()
+    {
+        if (!inCoRoutine)
+        {
+            StartCoroutine(DoSomething());
+        }
+    }
+
+   /* void newTarget()
+    {
+        float myX = gameObject.transform.position.x;
+        float myZ = gameObject.transform.position.z;
+
+        float xPos = myX + Random.Range(myX - 100, myX - 100);
+        float zPos = myZ + Random.Range(myZ - 100, myZ - 100);
+
+        Target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+
+        nav.SetDestination(Target);
+
+
+    }*/
+}
