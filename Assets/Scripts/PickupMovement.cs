2c62f0bb3cc50fdd0bc1ac182bd5b8d05fde154a
Yunip Shrestha <yunipshrestha@gmail.com>
2019-06-10 11:03:11 +0545

    Windows Build

--- Assets/Scripts/PickupMovement.cs
+++ Assets/Scripts/PickupMovement.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PickupMovement : MonoBehaviour
 {
     public int size;
+    //float timeCounter = 0;
+
     void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        /*timeCounter += Input.GetAxis("Horizontal") * Time.deltaTime; // multiply all this with some speed variable (* speed);
+        float x = Mathf.Cos(timeCounter);
+        float y = Mathf.Sin(timeCounter);
+        float z = 0;
+        transform.position = new Vector3(x, y, z);*/
     }
+
 }
